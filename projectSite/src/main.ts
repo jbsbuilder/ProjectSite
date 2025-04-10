64615c773ad4da876b8c29a74f65aed62ec6e366
Jacob Smith <work.jacobsmith@gmail.com>
2025-04-10 06:20:45 -0600

    amplify added

--- projectSite/src/main.ts
+++ projectSite/src/main.ts
@@ -6,11 +6,15 @@ import { createPinia } from 'pinia'
 import ui from '@nuxt/ui/vue-plugin'
 import App from './App.vue'
 import router from './router'
+import { Amplify } from "aws-amplify";
+import outputs from "amplify_outputs.json";
 
 const app = createApp(App)
-
+Amplify.configure({})
 app.use(createPinia())
 app.use(router)
 app.use(ui)
+
+Amplify.configure(outputs);
 
 app.mount('#app')
